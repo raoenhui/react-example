0e8e2e71182f5a96edc241b8e7ee25729aee8aea
raoenhui <raoenhui@jd.com>
2019-12-18 17:16:39 +0800

    feat: add changelog

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+/**
+ * 该 ESLint 配置扩展自 eslint-config-o2team-wx
+ * https://www.npmjs.com/package/eslint-config-o2team-wx
+ * o2team-wx 这一套规则是参考了 StandardJS 和 Airbnb 的 JS 规范，然后结合业务中的最佳实践整理输出的。
+ */
+module.exports = {
+    "extends": "react-app"
+}
